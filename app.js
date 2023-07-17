ad5d64a54274a870f042bc3c5b95a5df1ce426e5
Harshada Chame <harshadachame71@gmail.com>
2023-07-17 16:33:19 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const videoCardContainer = document.querySelector('.video-container');
 
-let api_key = "API KEY";
+let api_key ="api id";
 let video_http = "https://www.googleapis.com/youtube/v3/videos?";
 let channel_http = "https://www.googleapis.com/youtube/v3/channels?";
 
